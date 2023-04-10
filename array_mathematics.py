4f7220daf50c7f3c9d4cd498990fcb1203b03472
guilhermedom <guilhermedfs@gmail.com>
2023-04-09 21:21:34 -0300

    feat: add "Numpy" solution

---
+++ array_mathematics.py
@@ -0,0 +1,14 @@
+import numpy
+
+
+N, _ = map(int, input().split())
+
+A = numpy.array([input().split() for _ in range(N)], int)
+B = numpy.array([input().split() for _ in range(N)], int)
+
+print(A + B)
+print(A - B)
+print(A * B)
+print(A // B)
+print(A % B)
+print(A ** B)
